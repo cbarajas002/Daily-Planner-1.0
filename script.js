60517bbb1ff80389cccc2be15e5af43d50c7d127
Carlos Barajas <CBarajas002@gmail.com>
2021-10-18 20:18:03 -0700

    added the current time function to my javaScript & started to add my input blocks in my HTML & created my CSS and added a few chnages.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const timeDisplayEl = $('#time-display');
 
+
+
+function displayTime() {
+    const rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+    timeDisplayEl.text(rightNow);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+setInterval(displayTime, 1000);
